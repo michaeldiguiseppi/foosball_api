3634719670228873b8326d662211fe4b987fe5a3
Michael DiGuiseppi <michael.diguiseppi@echostar.com>
2017-10-16 13:28:43 -0600

    Removed client directory

--- src/server/app.js
+++ src/server/app.js
@@ -21,8 +21,6 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../client')));
-
 
 // *** main routes *** //
 app.use('/', routes);
